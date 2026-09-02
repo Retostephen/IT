d99f2b28f0698f521581a5fa78aa6c0eb19dc7b4
Retostephen <retostephen2006@gmail.com>
2026-09-02 11:22:19 +0100

    assignment 3 on how to receive user input done

---
+++ assignment-03-profile.py
@@ -0,0 +1,27 @@
+"""
+Assignment 3: User Input
+Student Registeration System
+"""
+
+name = input("Enter Name: ")
+age = int(input("Enter your age (They must be integers): "))
+department = input("Enter your Department: ")
+email = input("Enter your Email: ")
+phoneNumber = input("Enter your Phone Number: ")
+school = input("Enter name of school: ")
+
+print()
+print("=" * 26)
+print("STUDENT REGISTERATION")
+print("=" * 26)
+print()
+print(f"Welcome {name}")
+print()
+print("You have successfully regisetred.")
+print()
+print("Department:")
+print(department)
+print()
+print("Email:")
+print(email)
+
