483bc1a1020128bed4369d71d5c1bd91250135c2
Retostephen <retostephen2006@gmail.com>
2026-09-02 10:39:36 +0100

    Completed assignment 1 with objective of lening how to print an output

---
+++ assignment-01-profile.py
@@ -0,0 +1,18 @@
+"""
+Assignment 1: First Python Program
+Objective: Learn how to print output
+"""
+
+print("*" * 25)
+print("     STUDENT PROFILE")
+print("*" * 25)
+print()
+print("Name: JohN Doe")
+print()
+print("School: ABC University")
+print()
+print("Department: Computer Science")
+print()
+print("Favourite Food: Rice")
+print()
+print("Favourite Programming Language: Python")
