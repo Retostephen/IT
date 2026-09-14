75411b3468ba1e223bd5edaa44ff2ebe7447a9fd
Retostephen <retostephen2006@gmail.com>
2026-09-14 17:56:13 +0100

    Worked on the simple banking syystem and made sure all validations are properly placed

--- assignment-06-profile.py
+++ assignment-06-profile.py
@@ -15,27 +15,44 @@ while True:
 	print("3. Withdraw")
 	print("4. Exit")
 
-	choice = int(input("Choose an option(1-4): "))
+	choice = input("Choose an option(1-4): ")
 
-	if choice == 1:
+	if choice == "1":
 		print(f"Your balance is {balance}")
-	elif choice == 2:
-		amount = float(input("Enter amount to deposit: "))
-		if amount <= 0:
-			print("Invalid Amount to Deposit")
-		else:
-			balance += amount
-			print(f"Deposit successful. New balance: {balance}")
-	elif choice == 3:
-		amount = float(input("Enter amount to withdraw: "))
-		if amount <= 0:
-			print("Invalid Amount to Withdraw")
-		elif balance < amount:
+	elif choice == "2":
+		amountInput = input("Enter amount to deposit: ")
+		while True:
+			try:
+				amount = float(amountInput)
+				if amount <= 0:
+					print("Amount must be greater than zero.")
+					amountInput = input("Enter amount to deposit: ")
+				else:
+					break
+			except ValueError:
+				print("Invalid input. Please enter a number.")
+				amountInput = input("Enter amount to deposit: ")
+		balance += amount
+		print(f"Deposit successful. New balance: {balance}")
+	elif choice == "3":
+		amountInput = input("Enter amount to withdraw: ")
+		while True:
+			try:
+				amount = float(amountInput)
+				if amount <= 0:
+					print("Invalid Amount to Withdraw. Amount must be greater than Zero.")
+					amountInput = input("Enter amount to withdraw: ")
+				else:
+					break
+			except ValueError:
+				print("Invalid input. Please enter a number.")
+				amountInput = input("Enter amount to withdraw")
+		if balance < amount:
 			print("Insufficient Funds")
 		else:
 			balance -= amount
 			print(f"Withdrawal successful. New Balance: {balance}")
-	elif choice == 4:
+	elif choice == "4":
 		print("Thank you for banking with us")
 		break
 	else:
